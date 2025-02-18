8e7dfec05ddc10bac53256b875f4a9f007e04681
socra167 <socra167@gmail.com>
2025-02-18 17:32:47 +0900

    chore: 프로젝트 초기 세팅

---
+++ backend/src/test/java/com/coffeebean/CoffeebeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeebean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeebeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
