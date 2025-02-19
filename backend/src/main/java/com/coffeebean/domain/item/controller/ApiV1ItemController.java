7740cd87bd5816ef7773510fe54b25aa7c49beee
GODAOS <jmskkr@gmail.com>
2025-02-19 19:43:24 +0900

    :sparkles: Feat : 상품 등록 - 상품등록 간단하게 구현

--- backend/src/main/java/com/coffeebean/domain/item/controller/ApiV1ItemController.java
+++ backend/src/main/java/com/coffeebean/domain/item/controller/ApiV1ItemController.java
@@ -1,7 +1,10 @@
 package com.coffeebean.domain.item.controller;
 
 import com.coffeebean.domain.item.service.ItemService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,20 @@ public class ApiV1ItemController {
 
     @Autowired
     private ItemService itemService;
+
+
+    record AddReqBody(
+            String name,
+            int price,
+            int stockQuantity,
+            String description
+    ){}
+
+    // 상품 등록
+    @PostMapping
+    public void addItem(@RequestBody @Valid AddReqBody reqBody){
+        itemService.addItem(reqBody.name(), reqBody.price(), reqBody.stockQuantity(), reqBody.description());
+    }
+
+
 }
--- backend/src/main/java/com/coffeebean/domain/item/service/ItemService.java
+++ backend/src/main/java/com/coffeebean/domain/item/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.coffeebean.domain.item.service;
 
+import com.coffeebean.domain.item.entity.Item;
 import com.coffeebean.domain.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,14 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    public Item addItem(String name, int price, int stockQuantity, String description) {
+        return ItemRepository.save(
+                Item.builder()
+                        .name(name)
+                        .price(price)
+                        .stockQuantity(stockQuantity)
+                        .description(description)
+                        .build()
+        );
+    }
 }
