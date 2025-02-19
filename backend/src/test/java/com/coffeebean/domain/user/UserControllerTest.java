2418328b2da3e4c235e0814de921ef1a086d846a
gwony <moonsky1124@naver.com>
2025-02-20 02:33:02 +0900

    💄Style: 불필요한 import문 삭제 #3

--- backend/src/test/java/com/coffeebean/domain/user/UserControllerTest.java
+++ backend/src/test/java/com/coffeebean/domain/user/UserControllerTest.java
@@ -12,12 +12,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
