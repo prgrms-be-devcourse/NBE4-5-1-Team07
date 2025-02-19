9f7b16f09f60994408d5def784ac3c3890152b6a
gwony <moonsky1124@naver.com>
2025-02-19 23:52:29 +0900

    🔧Chore : 비밀번호 인코더 bean 추가

--- backend/src/main/java/com/coffeebean/global/security/SecurityConfig.java
+++ backend/src/main/java/com/coffeebean/global/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -32,6 +34,12 @@ public class SecurityConfig {
 
 		return http.build();
 	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 	@Bean
 	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
