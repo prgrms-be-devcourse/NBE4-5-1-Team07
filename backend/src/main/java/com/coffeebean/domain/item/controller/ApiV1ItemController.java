51f07298f2da14b094c6663f772826ad9ccca519
GODAOS <jmskkr@gmail.com>
2025-02-19 23:28:10 +0900

    :recycle: Refactor: 상품 전체 조회 return 변경
    - code, msg, data-items 출력
    - ItemDto 추가

--- backend/src/main/java/com/coffeebean/domain/item/controller/ApiV1ItemController.java
+++ backend/src/main/java/com/coffeebean/domain/item/controller/ApiV1ItemController.java
@@ -1,7 +1,9 @@
 package com.coffeebean.domain.item.controller;
 
+import com.coffeebean.domain.item.dto.ItemDto;
 import com.coffeebean.domain.item.entity.Item;
 import com.coffeebean.domain.item.service.ItemService;
+import com.coffeebean.global.dto.RsData;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -33,8 +35,17 @@ public class ApiV1ItemController {
 
     // 상품 전체 조회
     @GetMapping
-    public List<Item> getItems() {
-        return itemService.getItems();
+    public RsData<List<ItemDto>> getItems() {
+
+        List<ItemDto> items = itemService.getItems().stream()
+                .map(ItemDto::new)
+                .toList();
+
+        return new RsData<>(
+                "200-1",
+                "전체 상품이 조회 되었습니다.",
+                items
+        );
     }
 
     // 상품 단건 조회
@@ -59,9 +70,10 @@ public class ApiV1ItemController {
             String description
     ) {
     }
+
     // 상품 수정
     @PutMapping("/{id}")
-    public void modifyItem(@PathVariable long id, @RequestBody ModifyReqBody reqBody){
+    public void modifyItem(@PathVariable long id, @RequestBody ModifyReqBody reqBody) {
         Item item = itemService.getItem(id).get();
         itemService.modifyItem(item, reqBody.name(), reqBody.price(), reqBody.stockQuantity(), reqBody.description());
 
---
+++ backend/src/main/java/com/coffeebean/domain/item/dto/ItemDto.java
@@ -0,0 +1,28 @@
+package com.coffeebean.domain.item.dto;
+
+import com.coffeebean.domain.item.entity.Item;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+public class ItemDto {
+
+    @NonNull
+    private Long id;
+
+    private String name; // 상품명
+
+    private int price; // 상품 가격
+
+    private int stockQuantity; // 상품 수량
+
+    private String description; // 상품 설명
+
+    public ItemDto(Item item) {
+        this.id = item.getId();
+        this.name = item.getName();
+        this.price = item.getPrice();
+        this.stockQuantity = item.getStockQuantity();
+        this.description = item.getDescription();
+    }
+}
