311b29a15d0a22b3d2390dc42b5c5e284ba30927
GODAOS <jmskkr@gmail.com>
2025-02-21 00:48:10 +0900

    ✨Feat : 관리자 페이지 메뉴 레이아웃 등록
    - 상품관리, 주문 관리, 문의에 대한 레이아웃 제작

--- frontend/src/app/admin/ClientPage.tsx
+++ frontend/src/app/admin/ClientPage.tsx
@@ -3,6 +3,15 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 export default function ClinetLayout() {
   const products = Array.from({ length: 8 }, (_, i) => ({
@@ -13,6 +22,45 @@ export default function ClinetLayout() {
 
   return (
     <>
+      <Menubar className="flex w-full box-content">
+        <MenubarMenu>
+          <MenubarTrigger>상품 관리</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              <Link href="/admin/addItem">새상품 등록</Link>
+            </MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>
+              <Link href="/admin/checkItems">재고 확인</Link>
+            </MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>
+              <Link href="/admin/modifyItems">상품 변경</Link>
+            </MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>
+            <Link href="/admin/OrderManagement">주문 관리</Link>
+          </MenubarTrigger>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>문의</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              <Link href="/admin/noticeManagement">공지사항 관리</Link>
+            </MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>
+              <Link href="/admin/questionManagement">질문 관리</Link>
+            </MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>
+              <Link href="/admin/reviewByItems">상품별 리뷰</Link>
+            </MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
       <div className="flex w-full justify-end">
         <div className="flex w-full max-w-sm items-center space-x-2">
           <Input
