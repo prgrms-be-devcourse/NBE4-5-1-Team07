192e977983b46fa5f8712ab6a3237e5911c3a5f8
GODAOS <jmskkr@gmail.com>
2025-02-20 10:19:27 +0900

    ✨Feat : RsData, ServiceException, GlobalExceptoinHandler 적용
    - response메세지를 위한 클래스 및 Exception클래스 적용

---
+++ backend/src/main/java/com/coffeebean/global/exception/ServiceException.java
@@ -0,0 +1,26 @@
+package com.coffeebean.global.exception;
+
+import com.coffeebean.global.dto.RsData;
+
+public class ServiceException extends RuntimeException {
+
+    private RsData<?> rsData;
+
+    public ServiceException(String code, String message) {
+        super(message);
+        rsData = new RsData<>(code, message);
+    }
+
+    public String getCode() {
+        return rsData.getCode();
+    }
+
+    public String getMsg() {
+        return rsData.getMsg();
+    }
+
+    public int getStatusCode() {
+        return rsData.getStatusCode();
+    }
+
+}
