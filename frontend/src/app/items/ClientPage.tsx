14682e34d25748b3ad1cf9f5037dd307863e11d1
sameom1048 <samvision99@naver.com>
2025-02-21 00:31:33 +0900

    ♻️ Refactor : 메인 페이지 footer부분 상품 목록 페이지로 이동

--- frontend/src/app/items/ClientPage.tsx
+++ frontend/src/app/items/ClientPage.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ClientPage from "../notice/ClientPage";
 
 export default function ClinetLayout() {
   const products = Array.from({ length: 8 }, (_, i) => ({
@@ -11,22 +13,30 @@ export default function ClinetLayout() {
 
   return (
     <>
-      <div className="">
-        <div className="pb-4">
-          상품 목록입니다. 상품을 누르면 상세 페이지로 이동합니다.
-        </div>
-        <ul className="grid grid-cols-4 gap-6 px-4 w-11/12 mx-auto">
-          {products.map((product) => (
-            <li
-              className="border-2 border-red-300 p-2 rounded-2xl  h-[200px] hover:bg-gray-100 flex items-center justify-center"
-              key={product.id}
-            >
-              <Link href={`/items/${product.id}`}>
-                {product.id}. {product.name} - {product.price}원
+      <div className="flex flex-col gap-4">
+        <div className="p-4">
+          <div className="pb-4">
+            상품 목록입니다. 상품을 누르면 상세 페이지로 이동합니다.
+          </div>
+          <ul className="grid grid-cols-4 gap-6 px-4 w-11/12 mx-auto">
+            {products.map((product) => (
+              <Link href={`/items/${product.id}`} key={product.id}>
+                <li className="border-2 border-blue-300 p-2 rounded-2xl  h-[200px] hover:bg-gray-100 flex items-center justify-center">
+                  {product.id}. {product.name} - {product.price}원
+                </li>
               </Link>
-            </li>
-          ))}
-        </ul>
+            ))}
+          </ul>
+        </div>
+        <div className="bg-gray-300 rounded-b-2xl py-6 flex flex-col gap-2">
+          <div className="pl-4 flex flex-row gap-4">
+            <Link href="/notice">
+              <Button className="bg-blue-300 w-[100px] ">공지사항</Button>
+            </Link>
+            <Button className="bg-blue-300 w-[100px] ">FAQ</Button>
+          </div>
+          <ClientPage />
+        </div>
       </div>
     </>
   );
