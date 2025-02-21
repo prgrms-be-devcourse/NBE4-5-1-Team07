7e33889cf4117469aececdae760d918c6a71dcf9
socra167 <socra167@gmail.com>
2025-02-21 15:31:01 +0900

    :bug: Fix: Question 작성자 author에 nullable 적용

--- backend/src/main/java/com/coffeebean/domain/question/question/entity/Question.java
+++ backend/src/main/java/com/coffeebean/domain/question/question/entity/Question.java
@@ -52,7 +52,7 @@ public class Question {
 	private Answer answer;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "author_id")
+	@JoinColumn(name = "author_id", nullable = false)
 	private User author;
 
 	@ManyToOne(fetch = FetchType.LAZY)
