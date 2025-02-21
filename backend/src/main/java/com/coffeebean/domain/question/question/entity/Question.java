38cd9cf74c32ae4823b8ffd50ebe1c74c9e2fa35
socra167 <socra167@gmail.com>
2025-02-21 12:18:00 +0900

    :sparkles: Feat: `Question` Entity에 `Item` 연관 추가

--- backend/src/main/java/com/coffeebean/domain/question/question/entity/Question.java
+++ backend/src/main/java/com/coffeebean/domain/question/question/entity/Question.java
@@ -4,15 +4,18 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.LastModifiedDate;
 
+import com.coffeebean.domain.item.entity.Item;
 import com.coffeebean.domain.question.answer.entity.Answer;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -42,6 +45,10 @@ public class Question {
 	@OneToOne
 	@JoinColumn(name = "answer_id")
 	private Answer answer;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id", nullable = false)
+	private Item item;
 
 	private LocalDateTime createDate;
 
