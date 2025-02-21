f83ab5355640c193397e8b98885c92eeb38b50e9
gwony <moonsky1124@naver.com>
2025-02-21 10:52:28 +0900

    ✨Feat: jwt 유효성 검사

--- backend/src/main/java/com/coffeebean/global/util/JwtUtil.java
+++ backend/src/main/java/com/coffeebean/global/util/JwtUtil.java
@@ -1,7 +1,10 @@
 package com.coffeebean.global.util;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.Map;
@@ -11,27 +14,46 @@ import javax.crypto.SecretKey;
 @Component
 public class JwtUtil {
     private static final String SECRET_KEY = "676e27c6-60e8-49c7-8c0f-adf8e7ecaa2e";
+    private static final long EXPIRATION_TIME = 1000L * 3600 * 24; // 1일 (24시간)
 
-    public static String createToken(String username) {
+    // JWT 생성 (추가적인 Claims 포함 가능)
+    public static String createToken(Map<String, Object> claims) {
         SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
         Date issuedAt = new Date();
-        Date expiration = new Date(issuedAt.getTime() + 1000L * 3600 * 24); // 1일 만료 설정
+        Date expiration = new Date(issuedAt.getTime() + EXPIRATION_TIME);
 
         return Jwts.builder()
-                .setSubject(username)
+                .setClaims(claims) // 추가적인 claims 포함
                 .setIssuedAt(issuedAt)
                 .setExpiration(expiration)
-                .signWith(secretKey)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public static Map<String, Object> getPayload(String jwtStr) {
+    // JWT 유효성 검사
+    public static boolean isValidToken(String token) {
+        try {
+            SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parse(token);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // JWT 페이로드 추출
+    public static Map<String, Object> getPayload(String token) {
         SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
-        return (Map<String, Object>) Jwts
-            .parser()
-            .verifyWith(secretKey)
-            .build()
-            .parse(jwtStr)
-            .getPayload();
+
+        return (Map<String, Object>) Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parse(token)
+                .getPayload();
     }
 }
