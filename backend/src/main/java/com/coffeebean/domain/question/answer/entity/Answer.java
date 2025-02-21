7768d8f6da9f2ee4dae7aa290dcbc5761b720e39
socra167 <socra167@gmail.com>
2025-02-21 15:41:08 +0900

    :sparkles: Feat: Answer 작성자를 저장하기 위해 User와 ManyToOne 관계 추가

--- backend/src/main/java/com/coffeebean/domain/question/answer/entity/Answer.java
+++ backend/src/main/java/com/coffeebean/domain/question/answer/entity/Answer.java
@@ -6,14 +6,17 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import com.coffeebean.domain.question.question.entity.Question;
+import com.coffeebean.domain.user.user.enitity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -39,6 +42,9 @@ public class Answer {
 	@OneToOne
 	@JoinColumn(name = "question_id")
 	private Question question;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User author;
 
 	@CreatedDate
 	private LocalDateTime createDate;
