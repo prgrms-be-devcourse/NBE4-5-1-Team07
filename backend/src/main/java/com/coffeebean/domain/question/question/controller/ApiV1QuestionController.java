7716b4bdb7ebe14c18c6ee623495dd389d152b5f
socra167 <socra167@gmail.com>
2025-02-21 12:08:09 +0900

    :sparkles: Feat: `ApiV1QuestionController`, `QuestionService`, `QuestionRepository` 추가

---
+++ backend/src/main/java/com/coffeebean/domain/question/question/controller/ApiV1QuestionController.java
@@ -0,0 +1,9 @@
+package com.coffeebean.domain.question.question.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/questions")
+public class ApiV1QuestionController {
+}
---
+++ backend/src/main/java/com/coffeebean/domain/question/question/service/QuestionService.java
@@ -0,0 +1,7 @@
+package com.coffeebean.domain.question.question.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionService {
+}
