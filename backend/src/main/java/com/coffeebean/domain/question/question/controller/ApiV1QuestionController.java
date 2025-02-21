94edd23be117d6b2eb1e0f9f7cd4472d61202d99
socra167 <socra167@gmail.com>
2025-02-21 14:13:14 +0900

    :sparkles: Feat: 상품에 대해 질문 등록 기능 구현

--- backend/src/main/java/com/coffeebean/domain/question/question/controller/ApiV1QuestionController.java
+++ backend/src/main/java/com/coffeebean/domain/question/question/controller/ApiV1QuestionController.java
@@ -1,9 +1,54 @@
 package com.coffeebean.domain.question.question.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coffeebean.domain.question.question.service.QuestionService;
+import com.coffeebean.domain.user.user.enitity.User;
+import com.coffeebean.domain.user.user.service.UserService;
+import com.coffeebean.global.dto.RsData;
+import com.coffeebean.global.exception.ServiceException;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/questions")
 public class ApiV1QuestionController {
+
+	private final QuestionService questionService;
+	private final UserService userService;
+
+	record WriteQuestionReqBody(
+		@NotNull(message = "질문할 상품이 선택되지 않았습니다.")
+		long itemId,
+		@NotBlank(message = "질문 제목을 입력하세요.")
+		String subject,
+		@NotBlank(message = "내용을 입력하세요.")
+		String content,
+		@NotBlank(message = "인증 정보가 없습니다.")
+		String authToken
+	) {
+	}
+
+	@PostMapping
+	public RsData<Void> writeQuestion(@RequestBody @Valid WriteQuestionReqBody writeQuestionReqBody) {
+		User actor = userService.getUserByAuthToken(writeQuestionReqBody.authToken());
+		User realActor = userService.findByEmail(actor.getEmail())
+			.orElseThrow(() -> new ServiceException("401-1", "인증 정보가 잘못되었습니다."));
+
+		questionService.writeQuestion(realActor, writeQuestionReqBody.itemId(), writeQuestionReqBody.subject(),
+			writeQuestionReqBody.content());
+
+		return new RsData<>(
+			"200-1",
+			"질문 작성이 완료되었습니다."
+		);
+	}
 }
--- backend/src/main/java/com/coffeebean/domain/question/question/service/QuestionService.java
+++ backend/src/main/java/com/coffeebean/domain/question/question/service/QuestionService.java
@@ -2,6 +2,33 @@ package com.coffeebean.domain.question.question.service;
 
 import org.springframework.stereotype.Service;
 
+import com.coffeebean.domain.item.entity.Item;
+import com.coffeebean.domain.item.service.ItemService;
+import com.coffeebean.domain.question.question.entity.Question;
+import com.coffeebean.domain.question.question.repository.QuestionRepository;
+import com.coffeebean.domain.user.user.enitity.User;
+import com.coffeebean.global.exception.DataNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class QuestionService {
+
+	private final QuestionRepository questionRepository;
+	private final ItemService itemService;
+
+	public void writeQuestion(User author, long itemId, String subject, String content) {
+		Item item = itemService.getItem(itemId)
+			.orElseThrow(() -> new DataNotFoundException("존제하지 않는 상품에 질문을 작성할 수 없습니다."));
+
+		Question question = Question.builder()
+			.author(author)
+			.item(item)
+			.subject(subject)
+			.content(content)
+			.build();
+
+		questionRepository.save(question);
+	}
 }
