042d675185756f9edac063dabd2bf22179ef0853
SUN <suunn001@gmail.com>
2025-02-22 20:42:44 +0900

    ♻️ Refactor: 로그인 후 쿠키에서 사용자 정보 추출 로직 간편화

--- backend/src/main/java/com/coffeebean/domain/user/user/repository/UserRepository.java
+++ backend/src/main/java/com/coffeebean/domain/user/user/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.coffeebean.domain.user.user.repository;
 
 import com.coffeebean.domain.user.user.enitity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    Long id(Long id);
 }
--- backend/src/main/java/com/coffeebean/global/security/SecurityConfig.java
+++ backend/src/main/java/com/coffeebean/global/security/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.coffeebean.global.security;
 
+import com.coffeebean.domain.user.user.repository.UserRepository;
 import com.coffeebean.global.app.AppConfig;
 
 import java.util.Arrays;
 
+import com.coffeebean.global.util.JwtAuthenticationFilter;
+import com.coffeebean.global.util.JwtUtil;
+import io.jsonwebtoken.Jwt;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,13 +17,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,10 +39,12 @@ public class SecurityConfig {
                                 .requestMatchers("/h2-console/**", "/api/v1/user/admin/login").permitAll()
                                 // "/api/v1/user/admin/**" 경로는 관리자만 접근 가능
                                 .requestMatchers("/api/v1/user/admin/**").hasAuthority("admin")
+                                .requestMatchers("/api/v1/users/mypage").authenticated() // 인증 필요
                                 // 그 외의 모든 경로는 인증 없이 접근 가능
                                 .anyRequest().permitAll()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class)
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
