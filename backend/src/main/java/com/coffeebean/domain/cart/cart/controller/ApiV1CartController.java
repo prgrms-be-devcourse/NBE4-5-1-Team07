f2b1fae74f56afefaffb6f48e3fe9d15911f2d24
socra167 <socra167@gmail.com>
2025-02-24 12:00:55 +0900

    :sparkles: Feat: 장바구니 전체 삭제 기능에 쿠키 인증 적용

--- backend/src/main/java/com/coffeebean/domain/cart/cart/controller/ApiV1CartController.java
+++ backend/src/main/java/com/coffeebean/domain/cart/cart/controller/ApiV1CartController.java
@@ -1,5 +1,17 @@
 package com.coffeebean.domain.cart.cart.controller;
 
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.coffeebean.domain.cart.cart.dto.CartItemDto;
 import com.coffeebean.domain.cart.cart.dto.CartListResponseDto;
 import com.coffeebean.domain.cart.cart.entity.Cart;
@@ -9,7 +21,6 @@ import com.coffeebean.domain.user.user.enitity.User;
 import com.coffeebean.domain.user.user.service.UserService;
 import com.coffeebean.global.annotation.Login;
 import com.coffeebean.global.dto.RsData;
-import com.coffeebean.global.exception.ServiceException;
 import com.coffeebean.global.util.CustomUserDetails;
 
 import jakarta.validation.Valid;
@@ -17,12 +28,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
-
-import java.util.List;
-
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -31,7 +36,6 @@ public class ApiV1CartController {
 
 	private final CartService cartService;
 	private final CartItemService cartItemService;
-	private final UserService userService;
 
 	record AddItemReqBody(
 		Long id,
@@ -59,14 +63,10 @@ public class ApiV1CartController {
 		);
 	}
 
-	record AuthReqBody(@NotBlank(message = "인증 정보가 없습니다.") String authToken) {
-	}
-
 	// 자신의 장바구니 전체 조회
 	@GetMapping
 	@Transactional
 	public RsData<CartListResponseDto> getCarts(@Login CustomUserDetails userDetails) {
-
 		User actor = User.builder()
 			.id(userDetails.getUserId())
 			.email(userDetails.getEmail())
@@ -134,11 +134,14 @@ public class ApiV1CartController {
 
 	// 자산의 장바구니 상품 전체 삭제
 	@DeleteMapping()
-	public RsData<Void> deleteCart(@RequestBody @Valid AuthReqBody authReqBody) {
-		User actor = userService.getUserByAuthToken(authReqBody.authToken());
-		User realActor = userService.findByEmail(actor.getEmail())
-			.orElseThrow(() -> new ServiceException("401", "인증 정보가 잘못되었습니다."));
-		Cart cart = cartService.getMyCart(realActor);
+	@Transactional
+	public RsData<Void> deleteCart(@Login CustomUserDetails userDetails) {
+		User actor = User.builder()
+			.id(userDetails.getUserId())
+			.email(userDetails.getEmail())
+			.build();
+
+		Cart cart = cartService.getMyCart(actor);
 
 		cartService.deleteCart(cart);
 
