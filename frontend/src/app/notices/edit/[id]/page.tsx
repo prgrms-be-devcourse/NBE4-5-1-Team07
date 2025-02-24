b4f77bbc1fb5b0545c0721e950a024e34820b93e
sameom1048 <samvision99@naver.com>
2025-02-24 22:19:53 +0900

    ♻️ Refactor: 기본 공지사항 수정 기능 삭제 및 버튼 삭제

--- frontend/src/app/notices/edit/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClientPage from "./ClientPage";
-
-export default function Page({ params }: { params: { id: number } }) {
-  return (
-    <>
-      <ClientPage id={params.id} />
-    </>
-  );
-}
