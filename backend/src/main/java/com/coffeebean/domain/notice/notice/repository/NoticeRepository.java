8bc0b4a53e7bf311c3934cf25f35b32ae90a4d31
sameom1048 <samvision99@naver.com>
2025-02-25 15:19:07 +0900

    :sparkles: Feat: 공지사항 역순 출력

--- backend/src/main/java/com/coffeebean/domain/notice/notice/repository/NoticeRepository.java
+++ backend/src/main/java/com/coffeebean/domain/notice/notice/repository/NoticeRepository.java
@@ -1,9 +1,21 @@
 package com.coffeebean.domain.notice.notice.repository;
 
 import com.coffeebean.domain.notice.notice.entity.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    default Page<Notice> findAllByOrderByCreateDateDesc(Pageable pageable) {
+        Pageable sortedPageable = PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                Sort.by(Sort.Direction.DESC, "createDate") // 최신순 정렬 추가
+        );
+        return findAll(sortedPageable);
+    }
 }
