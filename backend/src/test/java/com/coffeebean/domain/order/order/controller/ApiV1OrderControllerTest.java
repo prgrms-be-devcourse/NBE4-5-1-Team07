075b018efc4a4abd9520d84e6cf95e8be60e3892
socra167 <socra167@gmail.com>
2025-02-25 11:16:45 +0900

    :rotating_light: Test: 회원이 적립금을 사용하는 테스트 작성

--- backend/src/test/java/com/coffeebean/domain/order/order/controller/ApiV1OrderControllerTest.java
+++ backend/src/test/java/com/coffeebean/domain/order/order/controller/ApiV1OrderControllerTest.java
@@ -93,6 +93,54 @@ class ApiV1OrderControllerTest {
 	}
 
 	@Test
+	@DisplayName("회원은 적립금이 사용 가능한 경우, 적립금을 사용할 수 있다")
+	void createOrderByUser_usePoint() throws Exception {
+		ResultActions resultActions = mvc.perform(
+				post("/api/v1/orders")
+					.content("""
+						{
+						  "items": [
+						    {
+						      "id": 1,
+						      "count": 2
+						    },
+						    {
+						      "id": 2,
+						      "count": 3
+						    }
+						  ],
+						  "address": {
+						    "city": "서울",
+						    "street": "원두아파트 100동 1201호",
+						    "zipcode": "23578"
+						  },
+						  "email": "example@exam.com",
+						  "cartOrder": false,
+						  "point": 2000
+						}
+						""".trim().stripIndent())
+					.cookie(new Cookie("token", authToken))
+					.contentType(
+						new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+					)
+			)
+			.andDo(print());
+
+		resultActions
+			.andExpect(status().isCreated())
+			.andExpect(handler().handlerType(ApiV1OrderController.class))
+			.andExpect(handler().methodName("createOrder"))
+			.andExpect(jsonPath("$.code").value("201-1"))
+			.andExpect(jsonPath("$.msg").value("주문이 등록되었습니다."))
+			.andExpect(jsonPath("$.data.deliveryAddress.city").value("서울"))
+			.andExpect(jsonPath("$.data.deliveryAddress.street").value("원두아파트 100동 1201호"))
+			.andExpect(jsonPath("$.data.deliveryAddress.zipcode").value("23578"))
+			.andExpect(jsonPath("$.data.deliveryStatus").value("READY"))
+			.andExpect(jsonPath("$.data.orderStatus").value("ORDER"))
+			.andExpect(jsonPath("$.data.orderDate").isNotEmpty());
+	}
+
+	@Test
 	@DisplayName("비회원은 주문을 등록할 수 있다")
 	void createOrderByNotUser() throws Exception {
 		ResultActions resultActions = mvc.perform(
